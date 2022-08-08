22c88bdf68cf8f79f008c79b6de1d774216d12ab
Harinee68 <harin608@naver.com>
2022-08-08 15:26:01 +0900

    [ADD] static 폴더 생성 및 static 처리

--- receipt/receipt/settings.py
+++ receipt/receipt/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,7 +110,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'board', 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- receipt/receipt/urls.py
+++ receipt/receipt/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from board import views as board_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('board/', board_views.board, name='board'),
 ]
