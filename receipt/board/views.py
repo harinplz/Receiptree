a09aae9c00bc4df70a8e13729f1fd2a8d1720889
Harinee68 <harin608@naver.com>
2022-08-09 00:20:28 +0900

    [ADD] 영수증 글 메인 화면 태그 별로 영수증 글 뜨도록 구현 #3
    - 영수증 글 메인 화면 태그 별로 영수증 글 뜨도록 구현

--- receipt/board/views.py
+++ receipt/board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Board, Tag
 
 # Create your views here.
@@ -7,7 +7,14 @@ from .models import Board, Tag
 def board(request):
 
     board_list = Board.objects.all()
-    return render(request, 'receipt_01.html', {'board':board_list,})
+    keyword = request.POST.get('search_button')
+    tag = Tag.objects.filter(tagname = keyword)
+    post = Board.objects.filter(tags__in = tag).order_by('-date')
+
+    return render(request, 'receipt_01.html', {'board':board_list, 'post':post, 'keyword':keyword})
+
+
+
 
 
 def write(request):
