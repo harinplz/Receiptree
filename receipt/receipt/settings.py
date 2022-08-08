ce291db8327b4dae5115bf4fc4dea3b8c2aed9db
Harinee68 <harin608@naver.com>
2022-08-08 17:24:09 +0900

    [MOD] 이미지 파일 저장 위한 MEDIA_ROOT, MEDIA_URL 지정 #17

--- receipt/receipt/settings.py
+++ receipt/receipt/settings.py
@@ -119,6 +119,10 @@ STATICFILES_DIRS = (
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
+
+MEDIA_URL = "/media/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- receipt/receipt/urls.py
+++ receipt/receipt/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 from board import views as board_views
+from django.conf import settings #settings.py import
+from django.conf.urls.static import static # 'static' import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,6 @@ urlpatterns = [
     path('board/', board_views.board, name='board'),
     path('board/write', board_views.write, name='board_write'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
