5ec8fdd46c5de39cc9343105142723d4ef55bc7e
Jungmin <jungmin011107@gmail.com>
2022-08-09 20:32:44 +0900

    [MOD]mypage_05 css 및 urls 수정 #13

--- receipt/receipt/urls.py
+++ receipt/receipt/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 from board import views as board_views
+from account import views as account_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('board/', board_views.board, name='board'),
     path('board/write', board_views.write, name='board_write'),
+    path('mypage', account_views.mypage, name='mypage'),
 ]
