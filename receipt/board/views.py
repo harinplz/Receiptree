d1bbe72bc59e361001308fb64e4376540788e9dd
Harinee68 <harin608@naver.com>
2022-08-10 19:38:36 +0900

    [ADD] 영수증 글 작성 화면 영수증 입력 받는 기능 구현 #23
    - 영수증 글 작성 화면에서 지출 일시, 사용 금액, 사용처, 내용을 입력하면 receipt 클래스가 생성되어 데이터베이스에 저장되는 것 구현 완료
    - Board와 Receipt 클래스가 1:N 관계이므로 하나의 Board에 여러 개의 Receipt를 가질 수 있도록 구현 완료

--- receipt/board/views.py
+++ receipt/board/views.py
@@ -26,6 +26,8 @@ def board_write(request):
         tags = request.POST['tags'].split(',')
 
         board = Board()
+        
+
         board.title = request.POST['title']
         board.date = timezone.datetime.now()
         board.body = request.POST['body']
@@ -38,6 +40,37 @@ def board_write(request):
                 continue
             _tag, _ = Tag.objects.get_or_create(tagname=tag)
             board.tags.add(_tag)
+
+
+        dates = request.POST['date[]'].split(',')
+        dates = list(dates)
+        costs = request.POST['cost[]'].split(',')
+        costs = list(costs)
+        places = request.POST['place[]'].split(',')
+        places = list(places)
+        contents = request.POST['content[]'].split(',')
+        contents = list(contents)
+
+        for i in range(0, len(dates)):
+            receipt = Receipt()
+            receipt.user_no = request.user
+            receipt.board_no = board
+
+            receipt.use_date = dates[i]
+            receipt.cost = costs[i]
+            receipt.place = places[i]
+            receipt.body = contents[i]
+
+            receipt.save()
+
+
+        
+        #receipt.use_date = request.POST['date[]']
+        # receipt.cost = request.POST['cost']
+        # receipt.place = request.POST['place']
+        # receipt.body = request.POST['content']
+
+        #receipt.save()
 
         return redirect('board_write')
     return render(request, 'write_02.html')
