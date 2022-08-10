47811ffd5506b9b6ad25729de8f705b8cd3a053b
Harinee68 <harin608@naver.com>
2022-08-11 02:20:28 +0900

    [CHORE] 영수증 글 작성 화면 간단 수정 #23

--- receipt/board/views.py
+++ receipt/board/views.py
@@ -24,6 +24,7 @@ def board_write(request):
     if request.method == 'GET':
         return render(request, 'write_02.html')
     elif request.method == 'POST':
+        
         tags = request.POST['tags'].split(',')
 
         board = Board()
@@ -33,16 +34,18 @@ def board_write(request):
         board.date = timezone.datetime.now()
         board.body = request.POST['body']
         board.user_no = request.user
-        board.save()
 
-        for tag in tags:
-            tag = tag.strip()
-            if not tag:
-                continue
-            _tag, _ = Tag.objects.get_or_create(tagname=tag)
-            board.tags.add(_tag)
+        if board.title and board.date and board.body is not None:
+            board.save()
 
+            for tag in tags:
+                tag = tag.strip()
+                if not tag:
+                    continue
+                _tag, _ = Tag.objects.get_or_create(tagname=tag)
+                board.tags.add(_tag)
 
+        
         dates = request.POST['date[]'].split(',')
         dates = list(dates)
         costs = request.POST['cost[]'].split(',')
@@ -52,17 +55,19 @@ def board_write(request):
         contents = request.POST['content[]'].split(',')
         contents = list(contents)
 
-        for i in range(0, len(dates)):
-            receipt = Receipt()
-            receipt.user_no = request.user
-            receipt.board_no = board
+        if dates and costs and places and contents is not None:
 
-            receipt.use_date = dates[i]
-            receipt.cost = costs[i]
-            receipt.place = places[i]
-            receipt.body = contents[i]
+            for i in range(0, len(dates)):
+                receipt = Receipt()
+                receipt.user_no = request.user
+                receipt.board_no = board
 
-            receipt.save()
+                receipt.use_date = dates[i]
+                receipt.cost = costs[i]
+                receipt.place = places[i]
+                receipt.body = contents[i]
+
+                receipt.save()
 
 
         
