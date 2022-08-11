06fed471c2a4fa31ca5ec242f42d067f5fee765b
Harinee68 <harin608@naver.com>
2022-08-11 18:07:53 +0900

    [ADD] 영수증 상세화면 댓글 데이터 가져오기 구현 완료 #36
    - 댓글 작성자, 댓글 작성자 프로필 이미지, 댓글 내용, 댓글 쓴 날짜 데이터 가져와서 화면에 띄워주기

--- receipt/board/views.py
+++ receipt/board/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import redirect, render, get_object_or_404
 from .models import *
 from django.utils import timezone
 
+import json
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
 # Create your views here.
 
 #영수증 메인 화면
@@ -12,7 +17,8 @@ def board_main(request):
     keyword = request.POST.get('search_button')
     tag = Tag.objects.filter(tagname = keyword)
     post = Board.objects.filter(tags__in = tag).order_by('-date')
-    hot_list = Board.objects.all().order_by('-good_cnt')[0:4]
+    hot_list = Board.objects.all()[0:4]
+    #hot_list = Board.objects.all().order_by('-good_cnt')[0:4]
 
     return render(request, 'receipt_01.html', {'board':board_list, 'post':post, 'keyword':keyword, 'hot':hot_list})
 
@@ -93,6 +99,41 @@ def board_detail(request, board_id):
     for receipt in receipt_list:
         sum += receipt.cost
 
+    # 댓글
+    comment_list = Comment.objects.filter(board_no = board_id)
     #print(sum)
     #print(receipt_list)
-    return render(request, 'writeDetail_02-1.html', {'board': board, 'receipt':receipt_list, 'sum':sum, })
+    return render(request, 'writeDetail_02-1.html', 
+    {'board': board, 'receipt':receipt_list, 'sum':sum, 'comments' : comment_list, })
+
+
+# 좋은 소비예요, 나쁜 소비예요 구현
+# @login_required #로그인 한 사용자만이 접근 가능
+# @require_POST #POST request만 받기로 함
+# def board_good(request):
+#     pk = request.POST.get('pk', None)
+#     board = get_object_or_404(Board, pk=pk)
+#     board_good, board_good_created = board.good_user.get_or_create(user=request.user)
+
+#     if not board_good_created:
+#         board_good.delete()
+#         message = "좋은 소비예요 취소"
+#     else:
+#         message = "좋은 소비예요"
+
+#     context = {'good_count' : board.count_good_user,
+#                 'message' : message }
+
+#     return HttpResponse(json.dumps(context), context_type="application/json")    
+    #user = request.user
+
+    # if board.good_user.filter(id=user.id).exists(): #오류나면 id->user_no 로 변경
+    #     board.good_user.remove(user)
+    #     message = "'좋은 소비예요' 버튼을 취소하셨습니다."
+    # else:
+    #     board.good_user.add(user)
+    #     message = "'좋은 소비예요' 버튼을 누르셨습니다."
+
+    # context = {'good_count':board.count_good_user(), 'message':message}
+    # return HttpResponse(json.dumps(context), content_type="application/json")
+
