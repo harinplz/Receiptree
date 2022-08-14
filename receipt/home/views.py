a7ec1643cc5e230911f203407540b258be9219bb
Harinee68 <harin608@naver.com>
2022-08-14 20:33:39 +0900

    [ADD] 홈화면 영수증 파티 내역 가져오기 #48

--- receipt/home/views.py
+++ receipt/home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from board.models import Board
+from party.models import Team
 from django.db.models import Count
 
 # Create your views here.
 def home(request):
     hot_list = Board.objects.annotate(num_good = Count('good_user')).order_by('-num_good')[:4]
+    team_list = Team.objects.all()[:6] #팀 클래스 받아오기
     
-    return render(request, 'home_04.html', {'hot':hot_list})
+    return render(request, 'home_04.html', {'hot':hot_list, 'team':team_list})
