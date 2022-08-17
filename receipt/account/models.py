3cd5893e0e3d74883462891b4a7b5c48bf1a34b5
Harinee68 <harin608@naver.com>
2022-08-17 17:50:08 +0900

    [CHORE] 디폴트 이미지 설정

--- receipt/account/models.py
+++ receipt/account/models.py
@@ -1,5 +1,6 @@
 from distutils.command.upload import upload
 from email.mime import image
+from email.policy import default
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -13,7 +14,7 @@ class User(AbstractUser):
     #email 기본 제공
     notify_cnt = models.IntegerField(default=0)
     grade = models.CharField(max_length=40)
-    image = models.ImageField(null=True, blank=True, upload_to = "")
+    image = models.ImageField(null=True, blank=True, upload_to = "", default='static/img/default.png')
     team_no = models.ForeignKey('party.Team', on_delete=models.CASCADE, db_column='team_no', null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True)
 
@@ -33,6 +34,8 @@ class User(AbstractUser):
     def my_image(self):
         if self.image:
             return self.image.url
+        if not self.image:
+            return f'/static/img/default.png'
         return ''
 
 
